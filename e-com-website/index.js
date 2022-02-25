6907389afc90401a067c92f29a377679fd186695
chandan_naresh <chandansutradhar2@gmail.com>
2022-02-25 16:50:14 +0530

    post lunch code

---
+++ e-com-website/index.js
@@ -0,0 +1,10 @@
+function getUserLocation() {
+	// navigator.geolocation.getCurrentPosition(
+	// 	(pos) => {
+	// 		alert(pos.coords.latitude + ":" + pos.coords.longitude);
+	// 	},
+	// 	(err) => {
+	// 		console.log(err);
+	// 	},
+	// );
+}
