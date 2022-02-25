77d011b6181cafca2648e652f1a9ebd9b6feaef2
chandan_naresh <chandansutradhar2@gmail.com>
2022-02-25 10:02:49 +0530

    if condition demo

--- day3/basic.js
+++ day3/basic.js
@@ -1,15 +1,17 @@
 const ifDemo = (n1) => {
 	//function scope || local scope
-	if (n1 < 35) {
+	if (n1 >= 0 && n1 < 35) {
 		console.log("fail");
 	} else if (n1 >= 35 && n1 < 50) {
 		//block scope
 		console.log("pass");
-	} else if (n1 >= 50 && n1 < 65) {
+	} else if (n1 >= 50 && n1 <= 63) {
 		console.log("second grade");
-	} else {
+	} else if (n1 >= 64 && n1 <= 100) {
 		console.log("first grade");
+	} else {
+		console.log("please enter marks between 0-100");
 	}
 };
 
-ifDemo(32);
+ifDemo(70);
