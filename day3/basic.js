03beea573764d99f719e41e7a214889867b8da17
chandan_naresh <chandansutradhar2@gmail.com>
2022-02-25 10:28:15 +0530

    if else tereniery operation demo

--- day3/basic.js
+++ day3/basic.js
@@ -14,4 +14,66 @@ const ifDemo = (n1) => {
 	}
 };
 
-ifDemo(70);
+const switchDemo = (score) => {
+	switch (score) {
+		case 30:
+			console.log("fail");
+			break;
+		case 50:
+			console.log("pass");
+			break;
+		case 60:
+			console.log("2nd grade");
+			break;
+		case 70:
+			console.log("1st grade");
+			break;
+		default:
+			console.log("unable to determine grade");
+			break;
+	}
+};
+
+const renderNavBar = (role) => {
+	switch (role) {
+		case "admin":
+			//TODO render admin navbar
+			console.log("admin navbar rendered");
+			break;
+		case "instructor":
+			//TODO render admin navbar
+			console.log("instructor navbar rendered");
+			break;
+		case "coordinator":
+			//TODO render admin navbar
+			console.log("coordinator navbar rendered");
+			break;
+		case "participant":
+			//TODO render admin navbar
+			console.log("participant navbar rendered");
+			break;
+		default:
+			//TODO render guest navbar
+			console.log("guest navbar rendered");
+			break;
+	}
+};
+//ifDemo(70);
+
+const ifElseNew = (isAdmin) => {
+	if (isAdmin) {
+		console.log("admin user");
+		//alert("admin login");
+	} else {
+		console.log("non admin user");
+		//alert("non admin login");
+	}
+
+	//return isAdmin === true ? console.log("admin user") : console.log("non admin user");
+	isAdmin === true ? console.log("admin user") : console.log("non admin user");
+};
+
+ifElseNew(true);
+// switchDemo(35);
+
+// renderNavBar("instructor");
